2253eefc41814a203c106aceb317315722eb5614
D_Zucho <danny.nico.dev@outlook.com>
2021-09-07 06:33:48 -0700

    start firebase google auth login

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,14 @@ import './header.css'
 import {ReactComponent as DisneyIcon} from '../../assets/images/logo.svg'
 
 import Navbar from '../../components/navbar/Navbar'
+import { handleGoogleAuth } from '../../firebase/firebase.config'
+
 
 const Header = (props) => {
+
+    
+
+
     return ( 
         <div className="header-container">
             <header className='header'>
@@ -12,7 +18,7 @@ const Header = (props) => {
                     <DisneyIcon className='disney-icon' />
                 </div>
                 <Navbar className='navbar'/>
-                <div className="login-container">
+                <div onClick={handleGoogleAuth} className="login-container">
                     Login
                 </div>
             </header>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,51 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+    } from 'firebase/auth'
+
+// firebase config object
+const firebaseConfig = {
+  apiKey: "AIzaSyBk6MutvaZihWh0bJcdJCAx1FHRZIN78sU",
+  authDomain: "disneyplus-clone-42d5c.firebaseapp.com",
+  projectId: "disneyplus-clone-42d5c",
+  storageBucket: "disneyplus-clone-42d5c.appspot.com",
+  messagingSenderId: "925267438517",
+  appId: "1:925267438517:web:13b82fa5ec41722f1887fc"
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+
+// firebase database
+const db = getFirestore()
+
+// firebase authentication object
+export const auth = getAuth(app)
+
+// add googleAuth API object
+export const provider = new GoogleAuthProvider()
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+
+//setting language to default
+auth.languageCode = 'it'
+
+provider.setCustomParameters({
+    'prompt' : 'select_account'
+})
+
+
+export const handleGoogleAuth = () => {
+    signInWithPopup(auth, provider).then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
+    }) 
+}
+
+
+
+export default db;
