97990388856eadc929b17d67565e4a8645baf2c1
D_Zucho <danny.nico.dev@outlook.com>
2021-09-12 08:34:11 -0700

    add carousel to user home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
 import UserHome from './pages/userHome/UserHome'
 import './App.css'
--- src/components/imageSlider/ImageSlider.jsx
+++ src/components/imageSlider/ImageSlider.jsx
@@ -1,9 +1,80 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Slider from 'react-slick'
+import styled from 'styled-components'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import './imageSlider.css'
+import sliderImage1 from '../../assets/images/slider-badag.jpg'
+import sliderImage2 from '../../assets/images/slider-badging.jpg'
+import sliderImage3 from '../../assets/images/slider-scale.jpg'
+import sliderImage4 from '../../assets/images/slider-scales.jpg'
+
 const ImageSlider = (props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: false,
+    prevArrow: false,
+  }
+
   return (
     <div className="img-slider-main-container">
-      <h1>hello</h1>
+      <Carousel className="slick-carousel" {...settings}>
+        <div className="carousel-item-wrapper">
+          <a>
+            <img
+              src={sliderImage2}
+              className="slider-image"
+              alt="slider-badging"
+              width="100%"
+            />
+          </a>
+        </div>
+
+        <div className="carousel-item-wrapper">
+          <a>
+            <img
+              src={sliderImage1}
+              className="slider-image"
+              alt="slider-badag"
+              width="100%"
+            />
+          </a>
+        </div>
+
+        <div className="carousel-item-wrapper">
+          <a>
+            <img
+              src={sliderImage3}
+              className="slider-image"
+              alt="slider-scale"
+              width="100%"
+            />
+          </a>
+        </div>
+
+        <div className="carousel-item-wrapper">
+          <a href="#">
+            <img
+              src={sliderImage4}
+              className="slider-image"
+              alt="slider-scales"
+              width="100%"
+            />
+          </a>
+        </div>
+      </Carousel>
     </div>
   )
 }
+
+const Carousel = styled(Slider)`
+  margin-top: 1.5rem;
+  height: 200px;
+`
 
 export default ImageSlider
